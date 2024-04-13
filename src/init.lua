89cd13c9637485c8b7bcf5d9223f49fcf50eda54
alice <166900055+alicesaidhi@users.noreply.github.com>
2024-04-13 08:42:25 +0200

    update requires and toml

--- src/init.lua
+++ src/init.lua
@@ -1,5 +1,66 @@
+local background = require(script.components.background)
+local bullet_list = require(script.components.bullet_list)
+local button = require(script.components.button)
+local class_icon = require(script.components.class_icon)
+local container = require(script.components.container)
+local dropdown = require(script.components.dropdown)
+local foreground = require(script.components.foreground)
+local image = require(script.components.image)
+local infinite_list = require(script.components.infinite_list)
+local instance_tree = require(script.components.instance_tree)
+local list = require(script.components.list)
+local loading_circle = require(script.components.loading_circle)
+local padding = require(script.components.padding)
+local pane = require(script.components.pane)
+local popup = require(script.components.popup)
+local portal = require(script.components.portal)
+local radio_button = require(script.components.radio_button)
+local radio_list = require(script.components.radio_list)
+local rounded_frame = require(script.components.rounded_frame)
+local row = require(script.components.row)
+local scroll_frame = require(script.components.scroll_frame)
+local shadow = require(script.components.shadow)
+local tabs = require(script.components.tabs)
+local text = require(script.components.text)
+local text_input = require(script.components.text_input)
+local toast_notification = require(script.components.toast_notification)
+local toggle = require(script.components.toggle)
+local transparent_layer = require(script.components.transparent_layer)
+local tree = require(script.components.tree)
+local color = require(script.util.color)
+
 return {
-	hello = function()
-		print("Hello world, from vide_studio_components!")
-	end,
+	
+	background = background,
+	bullet_list = bullet_list,
+	button = button,
+	class_icon = class_icon,
+	container = container,
+	dropdown = dropdown,
+	foreground = foreground,
+	image = image,
+	infinite_list = infinite_list,
+	instance_tree = instance_tree,
+	list = list,
+	loading_circle = loading_circle,
+	padding = padding,
+	pane = pane,
+	popup = popup,
+	portal = portal,
+	radio_button = radio_button,
+	radio_list = radio_list,
+	rounded_frame = rounded_frame,
+	row = row,
+	scroll_frame = scroll_frame,
+	shadow = shadow,
+	tabs = tabs,
+	text_input = text_input,
+	text = text,
+	toast_notification = toast_notification,
+	toggle = toggle,
+	transparent_layer = transparent_layer,
+	tree = tree,
+
+	color = color
+
 }
